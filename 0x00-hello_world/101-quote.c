56ad58affc1185c54a026d5abcbab49c3661d26a
Bilal ENEOUISSER <eneouisserbilal@gmail.com>
2023-10-05 19:42:29 -0700

    add fils C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
-#include <unistd.h> // Include the necessary header file for the write function
-
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); // Use the write function to write to the standard error (file descriptor 2)
-  return (1); // Return 1 to indicate an error condition
+  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+  return (1);
 }
