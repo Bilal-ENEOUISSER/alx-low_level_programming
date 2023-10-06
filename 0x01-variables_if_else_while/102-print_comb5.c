c114316155f0009397ca863e4527e467179abb2a
Bilal ENEOUISSER <eneouisserbilal@gmail.com>
2023-10-06 15:30:22 -0700

    add files if else while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - prints all possible combinations of two two-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, j, k, l;
+int i, j;
 
-for (i = 0; i <= 9; i++)
+for (i = 0; i < 100; i++)
 {
-for (j = 0; j <= 9; j++)
+for (j = 0; j < 100; j++)
 {
-for (k = i; k <= 9; k++)
+if (i < j)
 {
-for (l = j + 1; l <= 9; l++)
-{
-putchar(i + '0');
-putchar(j + '0');
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
 putchar(' ');
-putchar(k + '0');
-putchar(l + '0');
-if (i != 9 || j != 8 || k != 9 || l != 9)
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
-}
-}
-}
 
+}
+}
 putchar('\n');
-
 return (0);
 }
