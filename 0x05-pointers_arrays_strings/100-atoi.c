13257f6ec46a1bd237f31e7ea1a0fc1f1a385abe
Bilal ENEOUISSER <eneouisserbilal@gmail.com>
2023-10-17 17:39:40 -0700

    add files

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
@@ -7,26 +8,33 @@
  */
 int _atoi(char *s)
 {
-int i = 0;
-int result = 0;
-int sign = 1;
-int found_digit = 0;
-while (s[i] != '\0')
+int i, d, n, len, f, digit;
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
 {
 if (s[i] == '-')
+++d;
+if (s[i] >= '0' && s[i] <= '9')
 {
-sign *= -1;
-}
-else if (s[i] >= '0' && s[i] <= '9')
-{
-result = result * 10 + (s[i] - '0');
-found_digit = 1;
-}
-else if (found_digit)
-{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
+f = 0;
 }
 i++;
 }
-return result * sign;
+if (f == 0)
+return (0);
+return (n);
 }
