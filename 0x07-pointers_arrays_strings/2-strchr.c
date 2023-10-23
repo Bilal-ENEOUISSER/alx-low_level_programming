bd81e599eadc4c5cf57c97cc9bd6a4443ba0397a
Bilal ENEOUISSER <eneouisserbilal@gmail.com>
2023-10-23 10:22:43 -0700

    add file strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Pointer to the first occurrence of c in s, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+return (&s[i]);
+i++;
+}
+return (0);
+}
