91630b1b1b73d3d3ca8e8bf8f923295cef317451
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2024-01-12 22:09:00 +0100

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 
-
 /**
  * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
  * @head: A pointer to the head of the dlistint_t list.
@@ -11,47 +10,27 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    /*Declare new node pointers.*/
-    dlistint_t *new, *last;
+	dlistint_t *new, *last;
 
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-    /*Allocate memory for the new node.*/
-    new = malloc(sizeof(dlistint_t));
-    /*Check if memory allocation failed*/
-    if (new == NULL)
-        /*Return NULL if allocation failed.*/
-        return (NULL);
+	new->n = n;
+	new->next = NULL;
 
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
 
-    /*Set the value of the new node to the provided integer.*/
-    new->n = n;
-    /*Set the next pointer of the new node to NULL.*/
-    new->next = NULL;
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	new->prev = last;
 
-
-    /*Check if the list is empty.*/
-    if (*head == NULL)
-    {
-        /*Set the previous pointer of the new node to NULL.*/
-        new->prev = NULL;
-        /*Update the head pointer to point to the new node.*/
-        *head = new;
-        /*Return the address of the new node.*/
-        return (new);
-    }
-
-
-    /*Initialize a pointer to traverse the list.*/
-    last = *head;
-    /*Find the last node in the list.*/
-    while (last->next != NULL)
-        last = last->next;
-    /*Set the next pointer of the last node to the new node.*/
-    last->next = new;
-    /*Set the previous pointer of the new node to the last node.*/
-    new->prev = last;
-
-
-    /*Return the address of the new node.*/
-    return (new);
+	return (new);
 }
